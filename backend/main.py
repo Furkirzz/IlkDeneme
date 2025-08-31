5b8bd4398524297035f3b67b1f0f927caa97ae6e
Adile06 <adile.karci@hotmail.com>
2025-08-31 18:30:38 +0300

    Deneme Adile Karci

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,7 @@ Kurulum:
         OPENAI_API_KEY="sk-..."
 
 Çalıştırma:
-    python ocr_gpt.py deneme deneme Adile Karci, Recep, Furkan
+    python ocr_gpt.py deneme deneme Adile Karci, Recep, Furkan, deneme1, deneme2
 """
 
 import sys
